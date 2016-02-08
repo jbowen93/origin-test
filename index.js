6ddfe9a9655c5e43ec4ee7dcc77fbf4683b9252f
jbowen93 <jbowen@apigee.com>
2016-02-08 11:47:42 -0700

    Fix 2

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 const restify = require('restify');
 
-function respond(req, res, next) {
-  res.send('hello ' + req.params.name);
-  next();
-}
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8081;
+var ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
 var server = restify.createServer();
-server.get('/hello/:test', respond);
-server.get('/', respond);
 
-server.listen(8080, function() {
-  console.log('%s listing at %s', server.name, server.url);
-})
+server.get('/hello/:test', function (req, res) {
+  res.send('Hello ' + req.params.test);
+});
+
+server.get('/', function(req, res) {
+  res.send('Hello Origin');
+});
+
+server.listen(port, ip, function(){
+  console.log('%s listening at %s', server.name, server.url);
+});
