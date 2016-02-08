cc85ac88b78b2f6b29bd40f5affef04dcd453655
jbowen93 <jbowen@apigee.com>
2016-02-08 10:11:22 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const restify = require('restify');
+
+function respond(req, res, next) {
+  res.send('hello ' + req.params.name);
+  next();
+}
+
+var server = restify.createServer();
+server.get('/hello/:test', respond);
+
+server.listen(8080, function() {
+  console.log('%s listing at %s', server.name, server.url);
+})
